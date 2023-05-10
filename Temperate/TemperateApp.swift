220df2bcb1fb03dcba03877a6b74299b008bc907
basker <social724@gmail.com>
2023-05-10 10:13:40 +0900

    Initial Commit

---
+++ Temperate/TemperateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperateApp.swift
+//  Temperate
+//
+//  Created by HYEONJUN PARK on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct TemperateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
